b009ac9b40edcca40c349b0435b1f2db2fceec88
siyandamazibuko <siyanda86@gmail.com>
2023-09-24 13:34:31 +0200

    Add files via upload

---
+++ src/CleanArchitecture.Application/Validators/Users/GetUsersQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Messages.Queries.Users;
+using FluentValidation;
+
+namespace CleanArchitecture.Application.Validators.Users
+{
+    public class GetUsersQueryValidator : AbstractValidator<GetUsersQuery>
+    {
+        public GetUsersQueryValidator()
+        {
+        }
+    }
+}
