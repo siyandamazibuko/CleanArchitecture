349d0cf004df764dfebd193b29df439339b09373
siyandamazibuko <siyanda86@gmail.com>
2023-09-24 10:14:18 +0200

    Update GetUsersQueryHandler.cs

--- src/CleanArchitecture.Application/Handlers/Users/GetUsersQueryHandler.cs
+++ src/CleanArchitecture.Application/Handlers/Users/GetUsersQueryHandler.cs
@@ -1,24 +1,37 @@
-﻿using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using CleanArchitecture.Domain.Repositories;
 using CleanArchitecture.Messages.Queries.Users;
 using CleanArchitecture.Messages.Responses.Users;
+using CleanArchitecture.Models.Users;
 using MediatR;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace CleanArchitecture.Application.Handlers.Users
 {
     public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, UsersResponse>
     {
         private readonly IMapper _mapper;
+        private readonly IUsersRepository _usersRepository;
 
-        public GetUsersQueryHandler(IMapper mapper)
+        public GetUsersQueryHandler(IUsersRepository usersRepository, IMapper mapper)
         {
+            _usersRepository = usersRepository;
             _mapper = mapper;
         }
 
         public async Task<UsersResponse> Handle(GetUsersQuery request, CancellationToken cancellationToken)
         {
-            return null;
+            var userEntities = await _usersRepository.GetUsers() ?? new List<Domain.Entities.User>();
+            var users = _mapper.Map<IList<UserInformation>>(userEntities);
+
+            return new UsersResponse
+            {
+                Users = users
+            };
         }
     }
 }
